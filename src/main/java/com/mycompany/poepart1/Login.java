adb1acb5b2545f9fdd5f043d46ae06d490c98b9e
Student <Student@LabVM2049939.pqb4fyafr1buxpl1uqgc4iq2af.jnbx.internal.cloudapp.net>
2026-09-08 20:34:13 +0200

    debugged and improved code for login class , as well as added comments to code

--- src/main/java/com/mycompany/poepart1/Login.java
+++ src/main/java/com/mycompany/poepart1/Login.java
@@ -9,38 +9,123 @@ package com.mycompany.poepart1;
  * @author Student
  */
 public class Login {
-     public String phoneNumber ;
-     public String userName;
-      public String password;
-      
-     //constructors
-      public Login (String phoneNumber,String userName,String password){
-      this.userName=userName;
-      this.phoneNumber=phoneNumber;
-      this.password=password;
-      }
-      public boolean checkUserName(){
-          return userName != null && userName.contains("_") && userName.length() <6;
-       
-      }
-      public boolean checkCellPhoneNumber(){
-      return  phoneNumber!= null && phoneNumber.startsWith("ST") && phoneNumber.length()==10;
-      
-      }
-      public boolean checkPasswordComplexity(){
-      return password.contains(".*[A-Z].*") && password.contains(".*[0-9].*") && password.contains(".*[!@#$%^].*") && password.length()>=8;
-      }
-      public String registerUser(){
-      if(checkUserName() && checkPasswordComplexity()){
-         return "welcome" + userName + "it is great to see you again";
-      }else return "username or password incorrect, please try again";
-      }
-     public boolean loginUser(){
-     
-     
-     }
-     }
-     public String returnLoginStatus(){
-     
-     }
+    private String cellPhoneNumber;
+    private String username;
+    private String password;
+
+    public Login(String cellPhoneNumber, String username, String password) {
+        this.cellPhoneNumber = cellPhoneNumber;
+        this.username = username;
+        this.password = password;
+    }
+    /**
+     * Username must contain an underscore and be no more than five
+     * characters long.
+     */
+    public boolean checkUserName() {
+        return username != null
+                && username.contains("_")
+                && username.length() <= 5;
+    }
+
+    /**
+     * Password must be at least eight characters long and contain
+     * a capital letter, a number, and a special character.
+     */
+    public boolean checkPasswordComplexity() {
+        if (password == null || password.length() < 8) {
+            return false;
+        }
+        boolean hasUppercase = false;
+        boolean hasDigit = false;
+        boolean hasSpecialChar = false;
+
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+
+            if (Character.isUpperCase(c)) {
+                hasUppercase = true;
+            } else if (Character.isDigit(c)) {
+                hasDigit = true;
+            } else if (!Character.isLetterOrDigit(c)) {
+                // anything that isn't a letter and isn't a digit
+                // counts as a special character
+                hasSpecialChar = true;
+            }
+        }
+
+        return hasUppercase && hasDigit && hasSpecialChar;
+    }
+    /**
+     * Cell phone number must contain the South African international
+     * code (+27) followed by no more than ten digits.
+     */
+    public boolean checkCellPhoneNumber() {
+        if (cellPhoneNumber == null) {
+            return false;
+        }
+
+        if (!cellPhoneNumber.startsWith("+27")) {
+            return false;
+        }
+
+        // everything after the "+27" code
+        String numberPart = cellPhoneNumber.substring(3);
+
+        if (numberPart.length() == 0 || numberPart.length() > 10) {
+            return false;
+        }
+
+        // make sure the remaining characters are all digits
+        for (int i = 0; i < numberPart.length(); i++) {
+            if (!Character.isDigit(numberPart.charAt(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns the appropriate registration message depending on which
+     * validation checks pass or fail.
+     */
+    public String registerUser() {
+        if (!checkUserName()) {
+            return "Username is not correctly formatted; please ensure that your "
+                    + "username contains an underscore and is no more than five characters in length.";
+        }
+        if (!checkPasswordComplexity()) {
+            return "Password is not correctly formatted; please ensure that the "
+                    + "password contains at least eight characters, a capital letter, a number, "
+                    + "and a special character.";
+        }
+        if (!checkCellPhoneNumber()) {
+            return "Cell number is incorrectly formatted or does not contain an "
+                    + "international code; please correct the number and try again.";
+        }
+        return "Username successfully captured.\nPassword successfully captured.\n"
+                + "Cell number successfully captured.\nYou have been registered successfully, "
+                + username + "!";
+    }
+
+    /**
+     * Checks whether the username and password entered at login match
+     * the details this Login object was registered with.
+     */
+    public boolean loginUser(String enteredUsername, String enteredPassword) {
+        return username != null && username.equals(enteredUsername)
+                && password != null && password.equals(enteredPassword);
+    }
+
+    /**
+     * Returns the welcome message on successful login, or a failure
+     * message if the entered credentials don't match.
+     */
+    public String returnLoginStatus(String enteredUsername, String enteredPassword) {
+        if (loginUser(enteredUsername, enteredPassword)) {
+            return "Welcome " + username + ", it is great to see you again.";
+        }
+        return "Username or password incorrect, please try again.";
+    }
 }
